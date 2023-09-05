e85f27aecc8533e33fcee65cf869bc883221a247
Justin <57725347+juskek@users.noreply.github.com>
2023-09-05 17:04:54 +0100

    fix: no unique constraint matching given keys for referenced table "pokemon_species"

--- migrations/202308181628_create_pokemon_species_table.sql
+++ migrations/202308181628_create_pokemon_species_table.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS pokemon_species (
-    id UUID DEFAULT uuid_generate_v4(),
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     name TEXT UNIQUE NOT NULL
 );
