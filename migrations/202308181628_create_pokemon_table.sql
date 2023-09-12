ce2bfda3b9c118ee352046beb01bce23f31e3dcf
Justin <57725347+juskek@users.noreply.github.com>
2023-09-12 18:18:04 +0100

    refactor: remove unnecessary CREATE EXTENSION

--- migrations/202308181628_create_pokemon_table.sql
+++ migrations/202308181628_create_pokemon_table.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS pokemon (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     species_id UUID NOT NULL REFERENCES pokemon_species(id),
--- migrations/202309050000_create_pokemon_type_table.sql
+++ migrations/202309050000_create_pokemon_type_table.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 -- A species can belong to many types (e.g. `Bulbasaur` is grass and poison).
 -- A type relates to many species (e.g. both `Bulbasaur` and `Ivysaur` are grass type).
 -- Therefore, many-to-many relation between pokemon_type and pokemon_species
@@ -11,7 +9,7 @@ CREATE TABLE IF NOT EXISTS pokemon_type (
 
 -- Create junction table to relate pokemon_species and pokemon_type
 
-CREATE TABLE species_type (
+CREATE TABLE IF NOT EXISTS species_type (
     species_id UUID NOT NULL REFERENCES pokemon_species(id),
     type_id UUID NOT NULL REFERENCES pokemon_type(id),
     PRIMARY KEY (species_id, type_id)
